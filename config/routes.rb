ae7221a917eebad31024823e260a9afda1606523
Steve <sntakirutimana72@gmail.com>
2023-06-26 22:19:07 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, skip: :all
+  devise_scope :user do
+    post '/users/login', to: 'users/sessions#create', as: :user_session
+    post '/users', to: 'users/registrations#create', as: :user_registration
+    delete '/users/logout', to: 'users/sessions#destroy', as: :destroy_user_session
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/account', to: 'accounts#profile', as: :user_profile
+
+  namespace :v1 do
+    resources :chats_quarters, only: :create do
+      resources :messages, only: %i( index create )
+    end
+  end
+
+  mount ActionCable.server => '/cable'
 end
