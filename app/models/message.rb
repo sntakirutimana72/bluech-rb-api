c5e39cd8904f65d232f9f115e36be5ef2f49853d
Steve <sntakirutimana72@gmail.com>
2023-06-26 22:01:55 +0200

    Refactor Message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,13 @@
 class Message < ApplicationRecord
-  belongs_to :sender, class_name: 'User'
-  belongs_to :recipient, class_name: 'User'
+  belongs_to :author, class_name: 'User'
+  belongs_to :channel, class_name: 'ChatsQuarter'
 
   validates :desc, presence: true, length: { minimum: 1 }
+  validate :membership_required
+
+  private
+
+  def membership_required
+    errors.add(:channel_id, 'Must have membership') unless !channel.nil? && channel.member?(author)
+  end
 end
