cdbfa9319ec02d03faffd1c4c8296c99f0f45c07
Steve <sntakirutimana72@gmail.com>
2023-07-02 15:23:26 +0200

    Expand ApplicationController logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,19 @@
 class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
   include Responsible
 
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  respond_to :json
+
+  private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
+  end
+
+  def format_resource_errors(resource)
+    resource.errors.full_messages.join('\n\n')
   end
 end
