4f93838b4c891b18c6bdd39183fe8b7d9283223a
Steve <sntakirutimana72@gmail.com>
2023-07-26 13:19:28 +0200

    Remove :connected_at & :disconnected_at from migration file

--- db/migrate/20230725134700_add_channel_recaps_to_user.rb
+++
@@ -1,7 +0,0 @@
-class AddChannelRecapsToUser < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :connected_at, :timestamp
-    add_column :users, :disconnected_at, :timestamp
-    add_column :users, :online, :boolean, default: false, null: false
-  end
-end
