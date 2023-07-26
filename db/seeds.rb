05aee124ac4cbb68d503131a4c970854fb4c5a75
Steve <sntakirutimana72@gmail.com>
2023-07-26 13:24:34 +0200

    Generate migration file & seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,10 @@
-=begin
-  user_x = User.create(name: 'Steve', email: 'steve@email.test', password: 'pass@123')
-  user_y = User.create(name: 'Jean', email: 'jean@email.test', password: 'pass@123')
+users_meta = 5.times.map do |i|
+  { name: "Steve#{i + 1}", email: "steve#{i + 1}@gmail.com", password: 'pass@123' }
+end
 
-  x_y_channel = ChatsQuarter.create
-  x_y_channel.chats
-  x_y_channel.memberships
-  x_y_channel.members
-  x_y_channel.members << user_x
-  x_y_channel.members << user_y
+users = User.create(users_meta)
+recipient = users.first
 
-  x_channel = ChatsQuarter.create
-  x_channel << user_x
-
-  x_to_y = Message.create(desc: 'Hey there!', author: user_x, channel: x_y_channel)
-  y_to_x = Message.create(desc: 'Hi!', author: user_y, channel: x_y_channel)
-
-  user_x.messages
-  user_x.memberships
-  user_x.quarters
-=end
+users[1..].each do |author|
+  Message.create(rand(1..5).times.map { |i| { desc: "Hi-#{i + 1}!", recipient:, author: } })
+end
