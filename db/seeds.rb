a1268c3d1ad2f34d48a667d8c4cc68c18566fb77
Steve <sntakirutimana72@gmail.com>
2023-08-23 17:54:10 +0200

    Refactor database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,25 @@
-users_meta = 5.times.map do |i|
-  { name: "Steve#{i + 1}", email: "steve#{i + 1}@gmail.com", password: 'pass@123' }
+# Simulate users meta data
+def simulate_user(name)
+  { name:, email: "#{name.downcase}@gmail.com", password: 'pass@123' }
 end
 
-users = User.create(users_meta)
-recipient = users.first
+# Simulate messages meta data
+def simulate_chat(i: Integer, rec: User, author: User)
+  contents = %w(Hi Hello Hey Salute Greetings)
+  {
+    desc: "#{contents[i]}, #{rec.name}!",
+    recipient: rec,
+    author:
+  }
+end
 
+# Generate users
+users = User.create(%w(Tester Steve Eve).map(&method(:simulate_user)))
+
+# Pick a recipient user by random
+rec = users.first
+
+# Generate chat messages
 users[1..].each do |author|
-  Message.create(rand(1..5).times.map { |i| { desc: "Hi-#{i + 1}!", recipient:, author: } })
+  Message.create(rand(1..5).times.map { |i| simulate_chat(i:, rec:, author:) })
 end
