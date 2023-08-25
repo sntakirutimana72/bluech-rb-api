f72ddb25018946a082fdb179c589d44c777ed912
Steve <sntakirutimana72@gmail.com>
2023-08-25 21:05:54 +0200

    Re-evaluate :inbox scope query for accuracy

--- app/models/concerns/trackable.rb
+++ app/models/concerns/trackable.rb
@@ -9,24 +9,24 @@ module Trackable
       ).order(:created_at)
     }
 
-    scope :inbox, lambda { |rec_id|
+    scope :inbox, lambda { |recipient_id|
       select(
         :author_id,
         :desc,
         :created_at,
         'COUNT(messages.author_id) unread'
+      ).joins(
+        "INNER JOIN (select m1.author_id from messages m1 WHERE seen_at IS NULL AND recipient_id = #{recipient_id} AND author_id != #{recipient_id}) AS m1x ON m1x.author_id = messages.author_id"
+      ).where(
+        recipient_id:
+      ).having(
+        created_at: self.select('MAX(created_at)').where(recipient_id:, seen_at: nil).group(:author_id),
+        author_id: self.select('author_id').group(:author_id)
+      ).group(
+        :author_id, :created_at, :desc
+      ).order(
+        created_at: :desc
       )
-        .joins(
-          'LEFT JOIN (select A.author_id from messages A WHERE seen_at IS NULL) AS CX ON CX.author_id = messages.author_id'
-        )
-        .where(recipient_id: rec_id)
-        .where.not(author_id: rec_id)
-        .having(
-          created_at: self.select('MAX(created_at)').group(:author_id),
-          author_id: self.select('author_id').group(:author_id)
-        )
-        .group(:author_id, :created_at, :desc)
-        .order(created_at: :desc)
     }
   end
 end
