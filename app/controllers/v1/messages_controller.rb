3ea1c19310580c7f25d8ff8445fae59535274576
Steve <sntakirutimana72@gmail.com>
2023-08-26 21:11:15 +0200

    Add #mark_as_read controller handler to mark messages as read

--- app/controllers/v1/messages_controller.rb
+++ app/controllers/v1/messages_controller.rb
@@ -1,6 +1,7 @@
 module V1
   class MessagesController < ApplicationController
-    before_action :validate_convo_parameters, only: :index
+    before_action :validate_convo_params, only: :index
+    before_action :validate_seen_params, only: :mark_as_read
 
     def index
       @meta, @chats = pagy(Message.conversation(convo_params), page: page_num)
@@ -18,17 +19,32 @@ module V1
       as_created(message: MessageSerializer.new(@message))
     end
 
+    def mark_as_read
+      args = read_params.values
+
+      if args.first.length
+        args[0] = args.first.join(',')
+        ids = Message.mark_as_read(args)
+      else
+        ids = Message.mark_all_as_read(args[1..])
+      end
+
+      ChatsRelayJob.seen(*args[1..], ids)
+      as_success(ids:)
+    end
+
     private
+
+    def mark_as_read_params
+      params
+        .require(:convo)
+        .permit(ids: [], author_id: nil)
+        .merge(recipient_id: current_user.id)
+    end
 
     def page_num
       num = params.require(:convo)[:page].to_i
       num.zero? ? 1 : num
-    end
-
-    def validate_convo_parameters
-      return if params[:convo].is_a?(ActionController::Parameters) && params[:convo][:channelId].to_i.positive?
-
-      as_invalid(error: ':channelId required')
     end
 
     def convo_params
@@ -43,6 +59,22 @@ module V1
         .require(:message)
         .permit(:desc, :recipient_id)
         .merge(author: current_user)
+    end
+
+    def validate_convo_params
+      return if permittable?(:convo) && params[:convo][:channelId].to_i.positive?
+
+      as_invalid(error: ':channelId is required')
+    end
+
+    def validate_seen_params
+      return if permittable?(:convo) && params[:convo][:author_id].to_i.positive?
+
+      as_invalid(error: ':author_id is required')
+    end
+
+    def permittable?(key)
+      params[key].is_a?(ActionController::Parameters)
     end
   end
 end
