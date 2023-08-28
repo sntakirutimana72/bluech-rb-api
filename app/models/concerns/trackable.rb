2223e6abad2d7cc787bced324773045bb01ec3a2
Steve <sntakirutimana72@gmail.com>
2023-08-28 11:12:16 +0200

    Separate concerns logic

--- app/models/concerns/trackable.rb
+++ app/models/concerns/trackable.rb
@@ -28,23 +28,5 @@ module Trackable
         created_at: :desc
       )
     }
-
-    scope :mark_as_read, ->(binds) do
-      connection.exec_query(
-        "UPDATE messages SET seen_at = CURRENT_TIMESTAMP, updated_at = CURRENT_TIMESTAMP WHERE id = ANY(STRING_TO_ARRAY($1, ',')::INT[]) AND seen_at IS NULL AND author_id = $2 AND recipient_id = $3 RETURNING id;",
-        'MARK_SEEN',
-        binds,
-        prepare: true
-      )
-    end
-
-    scope :mark_all_as_read, ->(binds) do
-      connection.exec_query(
-        "UPDATE messages SET seen_at = CURRENT_TIMESTAMP, updated_at = CURRENT_TIMESTAMP WHERE seen_at IS NULL AND author_id = $1 AND recipient_id = $2 RETURNING id;",
-        'MARK_SEEN',
-        binds,
-        prepare: true
-      )
-    end
   end
 end
